e8c7a4f8c6a2fecab2b1398b883eff336edc2752
Edouard CHIN <chin.edouard@gmail.com>
2023-06-29 21:14:13 +0200

    Cleanup

--- activesupport/lib/active_support/new_tagged_logging.rb
+++ activesupport/lib/active_support/new_tagged_logging.rb
@@ -2,6 +2,7 @@
 
 require "active_support/core_ext/module/delegation"
 require "active_support/core_ext/object/blank"
+require "active_support/tagged_logging"
 require "logger"
 require "active_support/logger"
 
@@ -29,87 +30,10 @@ module ActiveSupport
   # to aid debugging of multi-user production applications.
   module TaggedLogging
     class TagProcessor
-      def tagged(*tags)
-        pushed_count = push_tags(tags).size
-        yield self
-      ensure
-        pop_tags(pushed_count)
-      end
+      include OldTaggedLogging::Formatter
 
       def call(msg)
         tag_stack.format_message(msg)
-      end
-
-      def push_tags(*tags)
-        tag_stack.push_tags(tags)
-      end
-
-      def pop_tags(count = 1)
-        tag_stack.pop_tags(count)
-      end
-
-      def clear_tags!
-        tag_stack.clear
-      end
-
-      def tag_stack
-        # We use our object ID here to avoid conflicting with other instances
-        @thread_key ||= "activesupport_tagged_logging_tags:#{object_id}"
-        IsolatedExecutionState[@thread_key] ||= TagStack.new
-      end
-
-      def current_tags
-        tag_stack.tags
-      end
-
-      def tags_text
-        tag_stack.format_message("")
-      end
-    end
-
-    class TagStack # :nodoc:
-      attr_reader :tags
-
-      def initialize
-        @tags = []
-        @tags_string = nil
-      end
-
-      def push_tags(tags)
-        @tags_string = nil
-        tags.flatten!
-        tags.reject!(&:blank?)
-        @tags.concat(tags)
-        tags
-      end
-
-      def pop_tags(count)
-        @tags_string = nil
-        @tags.pop(count)
-      end
-
-      def clear
-        @tags_string = nil
-        @tags.clear
-      end
-
-      def format_message(message)
-        if @tags.empty?
-          message
-        elsif @tags.size == 1
-          "[#{@tags[0]}] #{message}"
-        else
-          @tags_string ||= "[#{@tags.join("] [")}] "
-          "#{@tags_string}#{message}"
-        end
-      end
-    end
-
-    module LocalTagStorage # :nodoc:
-      attr_accessor :tag_stack
-
-      def self.extended(base)
-        base.tag_stack = TagStack.new
       end
     end
 
@@ -125,22 +49,22 @@ module ActiveSupport
 
     def self.new(logger)
       if logger.is_a?(TaggedLogging)
-        # ActiveSupport.deprecator.warn(<<~EOM)
-        #   `ActiveSupport::TaggedLogging.new` is deprecated.
-        #    To create a new logger from an existing logger, use `logger#clone` instead.
+        ActiveSupport.deprecator.warn(<<~EOM)
+          `ActiveSupport::TaggedLogging.new` is deprecated.
+           To create a new logger from an existing logger, use `logger#clone` instead.
 
-        #    Before: `new_tagged_logger = ActiveSupport::TaggedLogging.new(existing_logger)`
-        #    Now:    `new_tagged_logger = existing_logger.clone`
-        # EOM
+           Before: `new_tagged_logger = ActiveSupport::TaggedLogging.new(existing_logger)`
+           Now:    `new_tagged_logger = existing_logger.clone`
+        EOM
 
         logger.clone
       else
-        # ActiveSupport.deprecator.warn(<<~EOM)
-        #   To create a new Logger instance with Tagging functionalities, extend
-        #   the `ActiveSupport::TaggedLogging` module.
+        ActiveSupport.deprecator.warn(<<~EOM)
+          To create a new Logger instance with Tagging functionalities, extend
+          the `ActiveSupport::TaggedLogging` module.
 
-        #   Example: `my_logger.extend(ActiveSupport::TaggedLogging)`
-        # EOM
+          Example: `my_logger.extend(ActiveSupport::TaggedLogging)`
+        EOM
 
         logger.extend(TaggedLogging)
       end
@@ -156,25 +80,25 @@ module ActiveSupport
     end
 
     def add(*args, &block)
-      # if formatter.nil?
-      #   ActiveSupport.deprecator.warn(<<~EOM)
-      #     ActiveSupport::TaggedLogging will no longer set a default formatter on your logger.
-      #     To keep your logs unchanged in the future, use the `ActiveSupport::Logger` class or
-      #     set the `ActiveSupport::Logger::SimpleFormatter` formatter explicitely on the logger.
+      if formatter.nil?
+        ActiveSupport.deprecator.warn(<<~EOM)
+          ActiveSupport::TaggedLogging will no longer set a default formatter on your logger.
+          To keep your logs unchanged in the future, use the `ActiveSupport::Logger` class or
+          set the `ActiveSupport::Logger::SimpleFormatter` formatter explicitely on the logger.
 
-      #     Example:
+          Example:
 
-      #     logger = Rails::Logger.new
-      #     logger.extend(ActiveSupport::TaggedLogging)
+          logger = Rails::Logger.new
+          logger.extend(ActiveSupport::TaggedLogging)
 
-      #     Example:
+          Example:
 
-      #     custom_logger = CustomLogger.new(formatter: ActiveSupport::Logger::SimpleFormatter)
-      #     custom_logger.extend(ActiveSupport::TaggedLogging)
-      #   EOM
+          custom_logger = CustomLogger.new(formatter: ActiveSupport::Logger::SimpleFormatter)
+          custom_logger.extend(ActiveSupport::TaggedLogging)
+        EOM
 
         self.formatter ||= Logger::SimpleFormatter.new
-      # end
+      end
 
       super(*args, &block)
     end
@@ -185,7 +109,7 @@ module ActiveSupport
       else
         # Problem if the previous logger was part of a broadcast. See comment on #initialize_clone.
         logger = clone
-        logger.tag_processor.extend(LocalTagStorage)
+        logger.tag_processor.extend(OldTaggedLogging::LocalTagStorage)
         logger.tag_processor.push_tags(*tag_processor.current_tags, *tags)
 
         logger
